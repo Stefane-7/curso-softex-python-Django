7a41082f3a6a36cad95f732cde80616e1095d412
stefane <225245629+Stefane-7@users.noreply.github.com>
2025-08-27 11:42:44 -0300

    exercicio 03

---
+++ aula_08/exercicio_02.py
@@ -0,0 +1,30 @@
+"""Cire um programa que recebe um numero de telefone com 11 digitos
+
+1 - o numero é considerado invalido se tiver 3 numeros iguais.
+
+2 - O programa deve verificar se o numero tem 11digitos e se todos os caracteres são numeros.
+
+3 - se o numero for valido, o programa deve formata-lo para o padrão (xx) xxxx-xxxx.
+
+4 - O programa deve imprimir o numero formatado ou a mensagem de erro correspondente"""
+
+numero = input("Digite seu número de telefone: ")
+
+if len(numero) != 11:
+    print("Número com tamanho incorreto.")
+elif not numero.isdigit():
+    print("Não é possivel gerar um numero de telefone com esses valores")    
+else:
+    valido = True
+    for c in numero:
+        cont = 0
+        for d in numero:
+            if d == c:
+                cont += 1
+        if cont >= 3:
+            valido = False
+            break
+    if not valido:
+        print("Digite um telefone valido!")
+    else:
+        print("(" + numero[0]+numero[1] + ")" + numero[2]+numero[3]+numero[4]+numero[5]+numero[6] + "-" + numero[7]+numero[8]+numero[9]+numero[10])                    
---
+++ aula_08/exercicio_03.py
@@ -0,0 +1,18 @@
+"""crie um programa que codifica  e decodifica uma frase, seguindo as regras abaixo.
+cada vogal deve ser substituida pelo numero correspondente:
+a = 1
+e = 2
+i = 3
+o = 4
+u = 5
+
+o programa deve:
+1 - ler uma frase digitada pelo usuario.
+2 - exibir a frase codificada, trocando as vogais pelos numeros.
+3 - exibir a frase decodificada, voltando os numeros as vogais originais."""
+
+frase = input('Digite uma frase: ').lower
+nova_frase = frase.replace("a", "1").replace("e","2").replace("i","3").replace("o","4").replace("u","5")
+
+print(nova_frase)
+print(frase)
