ef92d9c9643eb2ace847c707a40e7e6e384eaa37
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-09-01 10:15:00 -0300

    .1

--- modulo_2/aula_01/exercicio_01.py
+++ modulo_2/aula_01/exercicio_01.py
@@ -1,1 +1,11 @@
-print("ok")
+"""Exercicio 1: Contando Ocorrências
+Crie um programa que conte quantas vezes um numero especifico aparece em uma lista.
+° ENTRADA: Uma lista de números e um número para ser procurado.
+° SAIDA: Um número inteiro que representa a quantidade de vezes que o número procurado aparece na lista.
+
+EXEMPLO: 
+numero = [1,5,2,8,5,3,5]
+numero_procurado = 5
+resultado esperado:
+3
+"""
