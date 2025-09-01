b451a073fc023a0e543966da4ee94507ce1f7fa4
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-09-01 10:44:42 -0300

    criando terceira lista

--- modulo_2/aula_01/exercicio_02.py
+++ modulo_2/aula_01/exercicio_02.py
@@ -1,7 +1,8 @@
 '''Nível 2: Combinando Lógica
 Aqui, você vai usar mais de uma lista e combinar diferentes lógicas de controle, como loops e
 verificações de pertencimento.
-Exercício 2: Encontrando Elementos Comuns
+
+     Exercício 2: Encontrando Elementos Comuns
 Você tem duas listas e precisa encontrar os elementos que aparecem em ambas. O programa
 deve gerar uma terceira lista contendo apenas os elementos em comum, sem repetições.
 ● Entrada: Duas listas.
@@ -11,3 +12,17 @@ lista1 = ["vermelho", "azul", "verde", "amarelo"]
 lista2 = ["verde", "roxo", "azul", "preto"]
 Resultado Esperado:
 ['azul', 'verde']'''
+
+
+lista1 = ["vermelho", "azul", "verde", "amarelo"]
+lista2 = ["verde", "roxo", "azul", "preto"]
+
+em_comum = []
+
+for item in lista1:
+    if item in lista2:
+        em_comum.append(item)
+print(lista2)
+print(lista1)
+print(f'Os itens em comum entre as listas são: {em_comum}')
+
