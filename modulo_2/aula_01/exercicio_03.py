4d24adf6117c14068742d6668ebd42e01eb665de
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-09-01 11:40:56 -0300

    explicado

--- modulo_2/aula_01/exercicio_03.py
+++ modulo_2/aula_01/exercicio_03.py
@@ -1,11 +1,15 @@
 """Nível 3: Lógica Avançada
 Neste nível, os desafios exigem mais de uma lista e combinam diferentes lógicas de controle,
 como loops e verificações de pertencimento.
-Exercício 3: Filtrando Números Primos
+   
+   Exercício 3: Filtrando Números Primos
 Sua tarefa é criar um programa que percorra uma lista de números e crie uma nova lista
 contendo apenas os números que forem primos.
-● Entrada: Uma lista de números inteiros.
-● Saída: Uma nova lista com os números primos encontrados.
+ 
+ ● Entrada: Uma lista de números inteiros.
+ 
+ ● Saída: Uma nova lista com os números primos encontrados.
+
 Exemplo:
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 Resultado Esperado:
@@ -15,5 +19,16 @@ Resultado Esperado:
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numeros_primos = []
 
-for num in numeros:
-    if num 
+for numero in numeros:
+    eh_primo = 2
+    if numero < 2:
+        eh_primo = False
+    else:
+        for i in range(2, numero):
+            if numero % i == 0:
+                eh_primo = False
+                break
+    if eh_primo:
+        numeros_primos.append(numero)                
+print(f'lista original: {numeros}')
+print(f'lista de númers primos: {numeros_primos}')        
