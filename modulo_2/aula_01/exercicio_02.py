102b7c96ef150b2a299c631412c2b28f5996a128
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-09-01 10:29:41 -0300

    .

---
+++ modulo_2/aula_01/exercicio_02.py
@@ -0,0 +1,13 @@
+'''Nível 2: Combinando Lógica
+Aqui, você vai usar mais de uma lista e combinar diferentes lógicas de controle, como loops e
+verificações de pertencimento.
+Exercício 2: Encontrando Elementos Comuns
+Você tem duas listas e precisa encontrar os elementos que aparecem em ambas. O programa
+deve gerar uma terceira lista contendo apenas os elementos em comum, sem repetições.
+● Entrada: Duas listas.
+● Saída: Uma nova lista com os elementos que as duas listas têm em comum.
+Exemplo:
+lista1 = ["vermelho", "azul", "verde", "amarelo"]
+lista2 = ["verde", "roxo", "azul", "preto"]
+Resultado Esperado:
+['azul', 'verde']'''
---
+++ modulo_2/aula_01/exercicio_03.py
@@ -0,0 +1,13 @@
+"""Nível 3: Lógica Avançada
+Neste nível, os desafios exigem mais de uma lista e combinam diferentes lógicas de controle,
+como loops e verificações de pertencimento.
+Exercício 3: Filtrando Números Primos
+Sua tarefa é criar um programa que percorra uma lista de números e crie uma nova lista
+contendo apenas os números que forem primos.
+● Entrada: Uma lista de números inteiros.
+● Saída: Uma nova lista com os números primos encontrados.
+Exemplo:
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Resultado Esperado:
+[2, 3, 5, 7]
+"""
