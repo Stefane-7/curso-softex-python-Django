0b8a291ea8ba2a2aa0352f53e339fa74265be8ac
stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-09-03 09:52:15 -0300

    tratamento de dados

---
+++ modulo_2/aula_02/exercicio_01.py
@@ -0,0 +1,17 @@
+"""Exercício 1: Filtragem e Análise de Dados de Vendas
+Você tem uma lista de vendas, onde cada venda é uma tupla (nome_do_produto, valor,
+quantidade).
+
+1. Crie uma nova lista chamada vendas_filtradas contendo apenas as tuplas onde o valor
+total da venda (valor * quantidade) é maior que 100.
+
+2. Crie um conjunto com todos os nomes de produtos únicos da lista original.
+O que vai entrar:
+vendas = [("Teclado", 50, 2), ("Mouse", 25.50, 4), ("Monitor", 300, 1), ("Fone", 45, 1),
+("Webcam", 75.20, 2)]
+
+A saída esperada:
+Vendas filtradas (valor total > 100):
+[('Teclado', 50, 2), ('Mouse', 25.5, 4), ('Monitor', 300, 1), ('Webcam', 75.2, 2)]
+Produtos únicos:
+{'Monitor', 'Fone', 'Mouse', 'Teclado', 'Webcam'}"""
