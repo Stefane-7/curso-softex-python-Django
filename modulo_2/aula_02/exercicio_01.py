e828439767bf03841864dad13369c0f6335ad33b
stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-09-03 10:04:06 -0300

    começo

--- modulo_2/aula_02/exercicio_01.py
+++ modulo_2/aula_02/exercicio_01.py
@@ -6,12 +6,24 @@ quantidade).
 total da venda (valor * quantidade) é maior que 100.
 
 2. Crie um conjunto com todos os nomes de produtos únicos da lista original.
+
 O que vai entrar:
 vendas = [("Teclado", 50, 2), ("Mouse", 25.50, 4), ("Monitor", 300, 1), ("Fone", 45, 1),
 ("Webcam", 75.20, 2)]
 
 A saída esperada:
-Vendas filtradas (valor total > 100):
+Vendas filtradas (valor total >= 100):
 [('Teclado', 50, 2), ('Mouse', 25.5, 4), ('Monitor', 300, 1), ('Webcam', 75.2, 2)]
 Produtos únicos:
 {'Monitor', 'Fone', 'Mouse', 'Teclado', 'Webcam'}"""
+
+vendas = [
+    ("Teclado", 50, 2),
+    ("Mouse", 25.50, 4),
+    ("Monitor", 300, 1),
+    ("Fone", 45, 1),
+    ("Webcam", 75.20, 2),
+]
+
+vendas_filtradas = []
+produtos_unicos = set()
