b474f74e68e8ffbbd5dd4a2a38d1fc6395791689
stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-09-03 11:46:15 -0300

    aula01

---
+++ modulo_01/aula_01/aula01.py
@@ -0,0 +1,3 @@
+idade = input("digite sua idade novamente:")
+idade_ajustada = int(idade)
+print(idade_ajustada + 8)
---
+++ modulo_01/aula_01/aula01parte2.py
@@ -0,0 +1,30 @@
+# soma = +
+# subtrcao = -
+#  multiplicacao = *
+# divisao = /
+#divisao inteira = //
+# resto da divisao = %
+# potencia = **
+
+numero = input("digite um numero:")
+numero_soma = int(numero)
+soma = numero_soma + 2
+print(soma)
+
+subtracao = numero_soma - 2
+print(subtracao)
+
+multiplicacao = numero_soma * 2
+print(multiplicacao)
+
+divisao = numero_soma / 2
+print(divisao)
+
+divisao-inteira = // 2
+print(divisao-inteira)
+
+resto-divisao = % 2
+print(resto-divisao)
+
+potencia = ** 2
+print(potencia)
