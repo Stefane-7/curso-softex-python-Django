9b2aa6ba818883ad9f2c5cc56833d642a5dcbdc5
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-04 16:54:42 -0300

    update modulo_2/desafios/Desafio_aula_02/desafio_aula_02.py

---
+++ modulo_2/desafios/Desafio_aula_02/desafio_aula_02.py
@@ -0,0 +1,30 @@
+dados_de_acesso = []
+while True:
+    try:
+      nome = input("Digite o nome de usuário (ou 'parar' para sair): ").title()
+      if nome.lower() == 'parar':
+        break
+      
+      status = input("Selecione o status (1 - Sucesso | 2 - Falha): ")  
+      if status == '1':  
+        status = 'Sucesso'  
+      elif status == '2':  
+        status = 'Falha'  
+      else:  
+        print("Opção inválida, tente novamente: ")  
+        continue  
+      duracao = int(input("Digite a duração da sessão em minutos: "))
+      if duracao < 0:
+        print("A duração não pode ser negativa.")
+        continue
+        
+      tuplas = (nome, status, duracao)  
+      dados_de_acesso.append(tuplas)
+      
+    except ValueError:
+       print("Digite apenas dados válidos")
+print("\nLista de tuplas: ")
+print(dados_de_acesso)
+
+
+
