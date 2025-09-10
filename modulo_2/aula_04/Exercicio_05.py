d2192162cde736097fc7447f57da0a497076e053
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-09-10 09:35:29 -0300

    .....

---
+++ modulo_2/aula_04/Exercicio_05.py
@@ -0,0 +1,5 @@
+def Verificador_de_pares(lista):
+  for numero in lista:
+    if numero % 2 == 0:
+        return numero
+  return None
---
+++ modulo_2/aula_04/exercicio_03.py
@@ -0,0 +1,5 @@
+def somar(a, b):
+    return a + b
+
+total = somar(5,3)
+print(total * 2)
