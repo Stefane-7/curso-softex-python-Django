40c29bab9a612ce8a391fb403699c7404f6dc8a5
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-11 10:49:04 -0300

    update modulo_2/desafios/desafio_aula_04/1_analisador_de_frases.py

---
+++ modulo_2/desafios/desafio_aula_04/1_analisador_de_frases.py
@@ -0,0 +1,7 @@
+"""Crie um programa que recebe uma frase do usuário e faz uma análise completa sobre ela,
+mostrando:
+● A quantidade de palavras na frase.
+● A quantidade de vogais (a, e, i, o, u).
+● A quantidade de consoantes.
+● Se a frase é um palíndromo (ou seja, se ela pode ser lida da mesma forma de trás para
+frente, ignorando espaços e letras maiúsculas)."""
