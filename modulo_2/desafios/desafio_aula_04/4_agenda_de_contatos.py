962bf5d95ec9417f8f83983b65f132e74d44d9b4
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-11 10:53:47 -0300

    update modulo_2/desafios/desafio_aula_04/4_agenda_de_contatos.py

---
+++ modulo_2/desafios/desafio_aula_04/4_agenda_de_contatos.py
@@ -0,0 +1,5 @@
+"""Crie um programa que funcione como uma agenda digital. Ele deve permitir adicionar,
+remover, procurar e listar contatos.
+Cada contato deve ser armazenado com nome, telefone e e-mail. O programa precisa ser
+robusto o suficiente para lidar com entradas inválidas do usuário, como tentar remover um
+contato que não existe."""
