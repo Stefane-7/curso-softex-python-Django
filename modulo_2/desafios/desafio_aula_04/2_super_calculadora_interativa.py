62613c9be7c491267e76ccda01bd28359445fe6a
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-11 10:49:41 -0300

    update modulo_2/desafios/desafio_aula_04/2_super_calculadora_interativa.py

---
+++ modulo_2/desafios/desafio_aula_04/2_super_calculadora_interativa.py
@@ -0,0 +1,6 @@
+"""Crie um programa que funcione como uma calculadora de bolso. Ele deve ser capaz de fazer
+adição, subtração, multiplicação e divisão.
+O programa deve sempre mostrar um menu de opções, pedir ao usuário para escolher a
+operação e digitar dois números. No final, ele exibe o resultado da conta. Se houver algum
+erro, como uma divisão por zero ou o usuário digitar algo que não é um número, o programa
+deve avisar e não travar."""
