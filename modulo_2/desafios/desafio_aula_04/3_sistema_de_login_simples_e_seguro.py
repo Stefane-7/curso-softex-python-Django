2782aee7cc60987cd8f244454313d5a71b0b30fe
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-11 10:53:50 -0300

    update modulo_2/desafios/desafio_aula_04/3_sistema_de_login_simples_e_seguro.py

---
+++ modulo_2/desafios/desafio_aula_04/3_sistema_de_login_simples_e_seguro.py
@@ -0,0 +1,6 @@
+"""Crie um sistema de cadastro e login de usuários. Ele deve:
+● Permitir que um usuário se registre com um nome e senha.
+● Validar a senha para garantir que ela seja segura (com pelo menos 8 caracteres e uma
+mistura de letras e números).
+● Permitir que o usuário faça login digitando seu nome e senha.
+● Informar se o login foi bem-sucedido ou falhou."""
