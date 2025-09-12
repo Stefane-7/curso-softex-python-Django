2f7804c11ae9182d26e937f5b042a25d9343df0c
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-12 11:41:14 -0300

    update modulo_2/desafios/desafio_aula_04/3_sistema_de_login_simples_e_seguro.py

--- modulo_2/desafios/desafio_aula_04/3_sistema_de_login_simples_e_seguro.py
+++ modulo_2/desafios/desafio_aula_04/3_sistema_de_login_simples_e_seguro.py
@@ -5,24 +5,43 @@ mistura de letras e números).
 ● Permitir que o usuário faça login digitando seu nome e senha.
 ● Informar se o login foi bem-sucedido ou falhou."""
 
-def sistema_login():
-  nome_salvo = "programação"
-  senha_salva = "python123"
-  
-  while True:
-    nome = input("Digite seu nome de usuario: ").lower()
-    senha = input("Digite sua senha: ")
-    senha_limpa = senha.strip()
-    if nome == nome_salvo:
-        if len(senha_limpa) >= 8 and senha_limpa.isalnum():
-            if senha_limpa == senha_salva:
-              print("Login bem-sucedido!!!")
-              return
-            else:
-                print("Login ou senha incorretos. Tente novamente.")
-        else:
-          print("Login ou senha incorretos. Tente novamente.")
-    else:
-      print("Login ou senha incorretos. Tente novamente.")
+def cadastrar_usuario():
+    nome = input("Cadastre seu nome de usuário: ").lower()
+    senha = input("Cadastre sua senha: ").strip()
 
-sistema_login()
+    while True:
+        tem_letra = False
+        tem_numero = False
+
+        for caractere in senha:
+            if caractere.isalpha():
+                tem_letra = True
+            if caractere.isdigit():
+                tem_numero = True
+
+        if len(senha) >= 8 and tem_letra and tem_numero:
+            print("Cadastro realizado com sucesso!")
+            return nome, senha
+        else:
+            print("A senha deve ter pelo menos 8 caracteres, incluindo letras e números.")
+            senha = input("Cadastre sua senha novamente: ").strip()
+
+
+def login_usuario(nome_salvo, senha_salva):
+    while True:
+        nome = input("Digite seu nome de usuário: ").lower()
+        senha = input("Digite sua senha: ").strip()
+
+        if nome == nome_salvo and senha == senha_salva:
+            print("Login bem-sucedido!!!")
+            return True
+        else:
+            print("Login ou senha incorretos. Tente novamente.")
+
+
+def sistema():
+    nome, senha = cadastrar_usuario
+    login_usuario(nome, senha)          # 
+
+
+sistema()
