7270c17342318bff778164fbb17be59572930f7a
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-14 20:39:22 -0300

    update modulo_2/desafios/desafio_aula_04/1_analisador_de_frases.py

--- modulo_2/desafios/desafio_aula_04/1_analisador_de_frases.py
+++ modulo_2/desafios/desafio_aula_04/1_analisador_de_frases.py
@@ -6,32 +6,36 @@ mostrando:
 ● Se a frase é um palíndromo (ou seja, se ela pode ser lida da mesma forma de trás para
 frente, ignorando espaços e letras maiúsculas)."""
 
-def verificador_palindromo(frase: str) -> bool:
-    frase = frase.lower().replace(" ", "")
-    if frase == frase[::-1]:
-      print("É um palíndromo? Sim")
-    else:
-      print("É um palíndromo? Não")
-    
-    
+
 def contador_vogais_consoantes(frase: str) -> None:
-  vogais = "a,e,i,o,u"
-  contador_vogais = 0
-  contador_consoates = 0
-  for letra in frase.replace(" ", ""):
-    if letra in vogais:
-      contador_vogais += 1
-      
-    elif letra not in vogais:
-      contador_consoates += 1
-  
-  print(f"Vogais: {contador_vogais}")
-  print(f"Consoantes: {contador_consoates}")
+    vogais = "aeiou"
+    contador_vogais = 0
+    contador_consoantes = 0
+
+    for letra in frase.replace(" ", ""):
+        if letra in vogais:
+            contador_vogais += 1
+        elif letra not in vogais and letra.isalpha():
+            contador_consoantes += 1
+
+    print(f"Vogais: {contador_vogais}")
+    print(f"Consoantes: {contador_consoantes}")
+
 
 def contador_palavras(frase: str) -> int:
-  palavras = len(frase.split())
-  print(f"Palavras: {palavras} ")
+    palavras = len(frase.split())
+    print(f"Palavras: {palavras}")
 
+
+def verificador_palindromo(frase: str) -> None:
+    frase_formatada = frase.lower().replace(" ", "")
+    if frase_formatada == frase_formatada[::-1]:
+        print("É um palíndromo? Sim")
+    else:
+        print("É um palíndromo? Não")
+
+
+# Programa principal
 frase = input("Digite uma frase: ")
 print("--- Resumo da Análise ---")
 contador_palavras(frase)
