31eff46eee28580571d8a963604b21457f8aded2
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-09-24 11:28:24 -0300

    exercicio 2

--- modulo_2/aula_08/exercicio_02.py
+++ modulo_2/aula_08/exercicio_02.py
@@ -10,7 +10,41 @@ incluir o nome do artista.
 
 ● Video deve ter um atributo adicional resolucao e sobrescrever o método exibir() para
 incluir a resolução.
+
 No script principal, crie um dicionário para organizar sua coleção de mídias, usando as
 chaves 'musicas' e 'videos'. Crie objetos de Musica e Video e os adicione a suas respectivas
 listas dentro do dicionário. 
 Por fim, itere sobre as listas e chame o método exibir() para cada item, demonstrando o polimorfismo de forma organizada."""
+
+class Midia:
+    def __init__(self, titulo, duracao_seg):
+        self.titulo = titulo
+        self.duracao_seg = duracao_seg
+
+    def exibir(self):
+        print(f"Titulo: {self.titulo} Duração: {self.duracao_seg} segundos")
+
+class Musica(Midia):
+    def __init__(self, titulo, duracao_seg, artista):
+        super().__init__(titulo, duracao_seg)
+        self.artista = artista
+
+    def exibir(self):
+        print(f"Titulo: {self.titulo} Duração: {self.duracao_seg} segundos. Artista: {self.artista}.") 
+
+class Video(Midia):
+    def __init__(self, titulo, duracao_seg, resolucao):
+        super().__init__(titulo, duracao_seg)
+        self.resolucao = resolucao
+
+    def exibir(self):
+        print(f"Titulo: {self.titulo} Duração: {self.duracao_seg} segundos. Resolução: {self.resolucao}")    
+
+musica = Musica("mmmmm", 400, "alguém")
+video = Video("AAAAAAAA", 5000, "3840x2160")
+
+acervo = {"musicas": [musica], "video": [video]}
+
+for i in acervo.values():
+    for i in i:
+        i.exibir()
