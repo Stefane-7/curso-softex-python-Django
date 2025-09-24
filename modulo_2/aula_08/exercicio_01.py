e87191a0e58a7eefaed6a95db45c77259b8efbc5
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-09-24 11:28:02 -0300

    exercicio 1

--- modulo_2/aula_08/exercicio_01.py
+++ modulo_2/aula_08/exercicio_01.py
@@ -9,3 +9,28 @@ deve sobrescrever o método apresentar() para incluir o curso na frase.
 
 Por fim, crie uma lista com um objeto Pessoa e um objeto Estudante. Itere sobre a lista e
 chame o método apresentar() para cada item, demonstrando o polimorfismo."""
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def apresentar(self):
+        print(f"Olá me chamo {self.nome} e tenho {self.idade} anos") 
+
+class Estudante(Pessoa):
+    def __init__(self, nome, idade, curso):
+        super().__init__(nome, idade)           
+        self.curso = curso
+
+    def apresentar(self):
+        print(f"Olá me chamo {self.nome} tenho {self.idade} e faço o curso: {self.curso}!")
+
+
+pessoa_1 = Pessoa("Ana", 35)
+pessoa_2 = Estudante("Paula", 28, "Contábeis")            
+
+lista = [pessoa_1, pessoa_2]
+
+for pessoa in lista:
+    pessoa.apresentar()
