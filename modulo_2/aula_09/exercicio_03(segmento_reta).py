89242eb0e5ddfa3adbe584763197635ed149335f
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-09-29 10:22:28 -0300

    ..

---
+++ modulo_2/aula_09/exercicio_03(segmento_reta).py
@@ -0,0 +1,21 @@
+"""3. Segmento de Reta (Fácil/Médio)
+
+● Classes: Ponto e SegmentoDeReta.
+
+● Classe Ponto:
+
+○ Atributos: x e y.
+
+○ Método: __init__(x, y).
+
+● Classe SegmentoDeReta:
+
+○ Atributos (Composição): ponto1 e ponto2, que devem ser instâncias de Ponto.
+
+○ Método: __init__(ponto1, ponto2).
+
+○ Método: calcular_comprimento() que retorna a distância entre os dois pontos.
+
+● Dica: Use o módulo math e a fórmula da distância euclidiana:
+(x2−x1)2+(y2−y1)2
+"""
