6a470017db4dc0739488589cce7510477d3d4429
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-29 13:20:57 -0300

    update modulo_2/aula_09/exercicio_05(casa_e_comodos).py

--- modulo_2/aula_09/exercicio_05(casa_e_comodos).py
+++ modulo_2/aula_09/exercicio_05(casa_e_comodos).py
@@ -19,11 +19,26 @@ na lista comodos.
 
 ○ Método: listar_comodos() que itera sobre a lista e imprime o nome de cada cômodo"""
 
+
 class Comodo:
     def __init__(self, nome):
         self.nome = nome
 
 class Casa:
-    def __init__(self):
-        pass        
-        
+    def __init__(self, comodos: list[Comodo]):
+        self.comodos = comodos
+    
+    def adicionar(self, comodo: Comodo):
+        self.comodos.append(comodo) 
+
+    def listar_comodos(self):
+        for comodo in self.comodos:
+            print(comodo.nome)
+
+comodo1 = Comodo('sala')
+comodo2 = Comodo('Cozinha')
+lista_comodos = [comodo1, comodo2]
+
+casa = Casa(lista_comodos)
+
+casa.listar_comodos()
