ff6cbd63b3b23c35824bca49b7642339a3f77630
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-10-01 12:53:23 -0300

    create modulo_03/aula_01/exercicio_02(revisao)/loja.py

---
+++ modulo_03/aula_01/exercicio_02(revisao)/loja.py
@@ -0,0 +1,8 @@
+"""Etapa 3: A Loja (no arquivo loja.py)
+Uma loja contém muitos produtos. Ela é como um grande contêiner que guarda tudo. Isso é o conceito de Composição.
+Seu trabalho aqui:
+● Crie uma classe chamada Loja.
+● Esta classe deve ter uma lista para guardar todos os objetos Produto.
+● Use um dicionário para gerenciar o estoque de cada produto, onde a "chave" é o nome do produto e o "valor" é a quantidade disponível.
+● Crie um método adicionar_produto_ao_estoque() que recebe um produto e a quantidade, e o adiciona à sua lista e ao seu dicionário de estoque.
+● Crie um método verificar_estoque_de_produto() que retorna a quantidade em estoque de um produto específico."""
