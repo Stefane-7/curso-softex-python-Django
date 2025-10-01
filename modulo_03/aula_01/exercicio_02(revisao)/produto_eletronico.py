b6baf750fcb9fac01c6306527939d65715bb8b24
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-10-01 12:59:48 -0300

    update modulo_03/aula_01/exercicio_02(revisao)/produto_eletronico.py

---
+++ modulo_03/aula_01/exercicio_02(revisao)/produto_eletronico.py
@@ -0,0 +1,6 @@
+"""
+Etapa 2: O Produto Eletrônico Especializado (no arquivo produto_eletronico.py)
+Um produto eletrônico é um produto, mas tem algumas características extras, como a garantia.
+Seu trabalho aqui:
+● Crie uma classe chamada ProdutoEletronico que herda (pega emprestado) todas as características da classe Produto. Isso é chamado de Herança.
+● Adicione um atributo tempo_garantia_meses a esta classe."""
