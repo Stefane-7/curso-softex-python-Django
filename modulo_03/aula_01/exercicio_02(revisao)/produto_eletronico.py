d5aa0bcf691906d407c7e4a63bce20834eff9c26
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-10-01 13:05:04 -0300

    update modulo_03/aula_01/exercicio_02(revisao)/produto_eletronico.py

--- modulo_03/aula_01/exercicio_02(revisao)/produto_eletronico.py
+++ modulo_03/aula_01/exercicio_02(revisao)/produto_eletronico.py
@@ -4,3 +4,10 @@ Um produto eletrônico é um produto, mas tem algumas características extras, c
 Seu trabalho aqui:
 ● Crie uma classe chamada ProdutoEletronico que herda (pega emprestado) todas as características da classe Produto. Isso é chamado de Herança.
 ● Adicione um atributo tempo_garantia_meses a esta classe."""
+
+from produto import produto
+class ProdutoEletronico(Produto):
+  def __init__(self, nome, preco, quantidade_em_estoque, tempo_garantia_meses):
+    super().__init__(nome, preco, quantidade_em_estoque)
+    self.tempo_garantia_meses = tempo_garantia_meses
+  
