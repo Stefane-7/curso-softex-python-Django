526558186f3f6c3fc6745bce5535a97b7efcabd8
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-10-08 11:54:11 -0300

    explicação aula 03

--- modulo_03/aula_03/aula.sql
+++ modulo_03/aula_03/aula.sql
@@ -18,6 +18,11 @@ INSERT INTO alunoss(nome, id_professor) VALUES ('João', 2);
 INSERT INTO alunoss(nome, id_professor) VALUES ('Maria', 2);
 INSERT INTO alunoss(nome, id_professor) VALUES ('Julia', 1);
 INSERT INTO alunoss(nome, id_professor) VALUES ('Pedro', 1);
+DELETE FROM alunos WHERE nome = 'Pedro';
 SELECT * FROM alunoss;
+SELECT id AS Identificador, nome AS Priemeiro_Nome FROM alunos;
 
-
+SELECT alunoss.nome AS nome_aluno, professores.nome AS nome_professor
+FROM alunoss
+INNER JOIN professores ON alunoss.id_professor = professores.id;
+DELETE FROM professores WHERE id = 2;
