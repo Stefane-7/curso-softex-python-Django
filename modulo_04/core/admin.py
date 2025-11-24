1ab39847fccaa477fdc234db7e40d7ba8a191949
fabiolobo17 <fabiolobo.ti@gmail.com>
2025-11-24 10:46:05 -0300

    ..

--- modulo_04/core/admin.py
+++ modulo_04/core/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Tarefa, Execucao # 1. Importe seu Model
 # Register your models here.
-admin.site.register(Tarefa)
-admin.site.register(Execucao)
+class TarefaAdmin(admin.ModelAdmin):
+    # 'list_display' é uma tupla com os nomes dos campos
+    # que queremos exibir como colunas na lista
+    list_display = ('titulo', 'user', 'concluida', 'criada_em')
+
+    # - 'concluida': Filtro de "Sim/Não"
+    # - 'user': Filtro de lista (baseado em um ForeignKey)
+    # - 'criada_em': Filtro de data (Hoje, Últimos 7 dias, Este mês, etc.)
+    list_filter = ('concluida', 'user', 'criada_em')
+
+admin.site.register(Tarefa, TarefaAdmin, Execucao)
