08009efbd3293c748c8a7af5d959922438009ea8
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-11-25 21:37:10 -0300

    novo painel admin

--- modulo_04/core/admin.py
+++ modulo_04/core/admin.py
@@ -2,13 +2,24 @@ from django.contrib import admin
 from .models import Tarefa # 1. Importe seu Model
 # Register your models here.
 class TarefaAdmin(admin.ModelAdmin):
-    # 'list_display' é uma tupla com os nomes dos campos
-    # que queremos exibir como colunas na lista
-    list_display = ('titulo', 'user', 'concluida', 'criada_em')
-
-    # - 'concluida': Filtro de "Sim/Não"
-    # - 'user': Filtro de lista (baseado em um ForeignKey)
-    # - 'criada_em': Filtro de data (Hoje, Últimos 7 dias, Este mês, etc.)
+    list_display = ('titulo', 'user', 'get_user_email','concluida', 'criada_em')
     list_filter = ('concluida', 'user', 'criada_em')
+    search_fields = ('titulo', 'user__username')
+
+    fieldsets = (
+        ('Informações Principais', {
+        'fields': ('user', 'titulo')
+        }),
+        ('Status da Tarefa', {
+        'fields': ('concluida', 'criada_em')
+        }),
+        )
+
+    readonly_fields = ('criada_em',)
+
+    @admin.display(description='Email do Usuário') 
+    def get_user_email(self, obj):
+        return obj.user.email
+
 
 admin.site.register(Tarefa, TarefaAdmin)
