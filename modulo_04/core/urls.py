1e988b8d763605443a5fc6ca54b74657ed6fa639
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-11-25 21:00:48 -0300

    atualizando models

--- modulo_04/core/urls.py
+++ modulo_04/core/urls.py
@@ -5,11 +5,12 @@ urlpatterns = [
 # Quando a URL for a raiz (''), chame a função 'home' de 'views.py'
     path('', views.home, name='home'),
     
-    path('home_2/', views.home_2, name='home_2'), 
-    
     path('editar/<int:id>/', views.editar_tarefa, name='editar_tarefa'), 
     
     path('excluir/<int:id>/', views.excluir_tarefa, name='excluir_tarefa'),
    
     path('alternar/<int:id>/', views.alternar_tarefa, name='alternar_tarefa'),
+    
+    path('register/', views.register, name='register'),
+
     ]
