1a9cbbd3b3193c813909f327f8471e0b67fa7670
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-12-08 09:48:15 -0300

    ..

--- modulo_05/core/views.py
+++ modulo_05/core/views.py
@@ -12,3 +12,26 @@ class ListaTarefasAPIView(APIView):
         serializer = TarefaSerializer(tarefas, many=True)
         
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, format=None):
+    
+        # 1. INSTANCIAR: Criar serializer com dados recebidos
+        serializer = TarefaSerializer(data=request.data)
+
+        # 2. VALIDAR: Checar se os dados são válidos
+        if serializer.is_valid():
+            # 3. SALVAR: Persistir no banco de dados
+            serializer.save()
+
+        # 4. RESPONDER: Retornar objeto criado + status 201
+            return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+            )
+
+        # 5. ERRO: Retornar erros de validação + status 400
+        return Response(
+        serializer.errors,
+        status=status.HTTP_400_BAD_REQUEST
+        )
+    
