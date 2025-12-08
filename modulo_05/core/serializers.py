f6c8836f17fd841e822928c50e3afd40a5d467bf
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-12-08 10:23:05 -0300

    adicionando validação no serializer

--- modulo_05/core/serializers.py
+++ modulo_05/core/serializers.py
@@ -8,3 +8,28 @@ class TarefaSerializer(serializers.ModelSerializer):
         fields = ['id', 'user', 'titulo', 'concluida', 'criada_em']
         # Campos gerados automaticamente (não aceitos na entrada)
         read_only_fields = ['id', 'criada_em']
+
+    def validate_titulo(self, value):
+   
+        value = value.strip()
+
+        # Validação 1: Não vazio
+        if not value:
+            raise serializers.ValidationError(
+            "O título não pode ser vazio ou conter apenas espaços."
+            )
+
+        # Validação 2: Mínimo de caracteres
+        if len(value) < 3:
+            raise serializers.ValidationError(
+            "O título deve ter pelo menos 3 caracteres."
+            )
+
+        # Validação 3: Não apenas números
+        if value.isdigit():
+            raise serializers.ValidationError(
+            "O título não pode conter apenas números."
+            )
+
+        return value
+
